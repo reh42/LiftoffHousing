4575a918db036daa0799cc4fc36d2c08b0791c07
reh42 <reharbison42@gmail.com>
2021-04-26 17:46:19 -0500

    first commit

---
+++ src/main/java/org/launchcode/liftoff_housing/LiftoffHousingApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.liftoff_housing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiftoffHousingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiftoffHousingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/liftoff_housing/LiftoffHousingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.liftoff_housing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiftoffHousingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
